c42f39836d5b4aac97d410ec990dfc333a81c3e2
Sergije <sergijeanticevic@gmail.com>
2024-07-31 12:43:56 +0200

    Fixed mobile tech

--- src/features/Tech/Tech.tsx
+++ src/features/Tech/Tech.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Image from 'next/image';
 import React from 'react';
+import { useInView } from 'react-intersection-observer';
 
 import Section from '@/common/Section';
 import TitleParagraph from '@/common/TitleParagraph';
@@ -9,6 +12,10 @@ import { cn } from '@/utils/classNames';
 import Technologies from './Technologies';
 
 const Tech = () => {
+  const { ref, inView } = useInView({
+    threshold: 0.3,
+  });
+
   return (
     <Section
       id='tech'
@@ -21,7 +28,9 @@ const Tech = () => {
       />
       <div className={cn('flex justify-center')}>
         <div
-          className={cn('relative translate-x-[-6%] md:translate-x-0 mx-[4px] my-[0px] sm:mx-[120px] sm:my-[200px]')}
+          className={cn(
+            'hidden lg:block relative translate-x-[-6%] md:translate-x-0 mx-[4px] my-[0px] sm:mx-[120px] sm:my-[200px]',
+          )}
         >
           <Image
             src='/assets/images/tech/spaceship.png'
@@ -30,6 +39,19 @@ const Tech = () => {
             height={320}
           />
           <Technologies />
+        </div>
+        <div
+          ref={ref}
+          className={cn('block lg:hidden relative md:translate-x-0 mx-[4px] my-[0px] opacity-0 duration-1000', {
+            'opacity-100': inView,
+          })}
+        >
+          <Image
+            src='/assets/images/tech/spaceship-mobile.png'
+            alt='Spaceship'
+            width={500}
+            height={320}
+          />
         </div>
       </div>
     </Section>
--- src/features/Tech/Technologies.tsx
+++ src/features/Tech/Technologies.tsx
@@ -1,53 +1,51 @@
-'use client';
-
 import Image from 'next/image';
 import React from 'react';
 import { useInView } from 'react-intersection-observer';
 
 import { cn } from '@/utils/classNames';
 
-const technologies = (isSm: boolean) => [
+const TECHNOLOGIES = [
   {
-    src: isSm ? '/assets/images/tech/metaplex-m.svg' : '/assets/images/tech/metaplex.png',
+    src: '/assets/images/tech/metaplex.png',
     alt: 'Metaplex',
-    width: isSm ? 54 : 208,
-    height: isSm ? 86 : 143,
-    classes: isSm ? 'left-[1%] top-[5%]' : 'left-[-23%] top-[-18%]',
+    width: 208,
+    height: 143,
+    classes: 'left-[-23%] top-[-18%]',
   },
   {
-    src: isSm ? '/assets/images/tech/react-m.svg' : '/assets/images/tech/react.png',
+    src: '/assets/images/tech/react.png',
     alt: 'React',
-    width: isSm ? 41 : 81,
-    height: isSm ? 132 : 200,
-    classes: isSm ? 'right-[3%] top-[-26%]' : 'right-[-8%] top-[-41%]',
+    width: 81,
+    height: 200,
+    classes: 'right-[-8%] top-[-41%]',
   },
   {
-    src: isSm ? '/assets/images/tech/rust-m.svg' : '/assets/images/tech/rust.png',
+    src: '/assets/images/tech/rust.png',
     alt: 'Rust',
-    width: isSm ? 52 : 223,
-    height: isSm ? 76 : 112,
-    classes: isSm ? 'right-[-12%] top-[26%]' : 'right-[-50%] top-[17%]',
+    width: 223,
+    height: 112,
+    classes: 'right-[-50%] top-[17%]',
   },
   {
-    src: isSm ? '/assets/images/tech/node-m.svg' : '/assets/images/tech/node.png',
+    src: '/assets/images/tech/node.png',
     alt: 'Node',
-    width: isSm ? 100 : 246,
-    height: isSm ? 62 : 155,
-    classes: isSm ? 'right-[-2%] bottom-[-21%]' : 'right-[-24%] bottom-[-50%]',
+    width: 246,
+    height: 155,
+    classes: 'right-[-24%] bottom-[-50%]',
   },
   {
-    src: isSm ? '/assets/images/tech/nextJs-m.svg' : '/assets/images/tech/nextJs.png',
+    src: '/assets/images/tech/nextJs.png',
     alt: 'NextJS',
-    width: isSm ? 70 : 140,
-    height: isSm ? 75 : 175,
-    classes: isSm ? 'right-[47%] bottom-[-27%]' : 'right-[47%] bottom-[-58%]',
+    width: 140,
+    height: 175,
+    classes: 'right-[47%] bottom-[-58%]',
   },
   {
-    src: isSm ? '/assets/images/tech/aws-m.svg' : '/assets/images/tech/aws.png',
+    src: '/assets/images/tech/aws.png',
     alt: 'AWS',
-    width: isSm ? 77 : 200,
-    height: isSm ? 48 : 110,
-    classes: isSm ? 'left-[1%] bottom-[-8%]' : 'left-[-23%] bottom-[-36%]',
+    width: 200,
+    height: 110,
+    classes: 'left-[-23%] bottom-[-36%]',
   },
 ];
 
@@ -60,7 +58,7 @@ const Technologies = () => {
 
   return (
     <div ref={ref}>
-      {technologies(window.innerWidth < 940).map(({ src, alt, width, height, classes }, i) => (
+      {TECHNOLOGIES.map(({ src, alt, width, height, classes }, i) => (
         <div
           key={alt}
           className={cn(classes, 'absolute z-1')}
